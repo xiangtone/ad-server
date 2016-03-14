01c95f9a3c77471c56c7277c759295ca99847cdc
soybean217 <13565644@qq.com>
2016-03-14 09:32:07 +0800

    修改shell执行命令，增加错误流的写入。

--- AdCommon/src/main/java/org/common/util/ad/ExecCommandUtils.java
+++ AdCommon/src/main/java/org/common/util/ad/ExecCommandUtils.java
@@ -6,90 +6,101 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 
+import org.apache.log4j.Logger;
+
 /**
  * 功能概述：<br>
  *
  */
 public class ExecCommandUtils {
-	/**
-	 * 执行外部命令
-	 * 
-	 * @param command
-	 */
-	public static void execCommand(final String command, String resultFilePath) {
-		try {
-			int exitVal = 53559;
 
-			Process process = Runtime.getRuntime().exec(command);
-			new WatchDog(process, command).start();
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					process.getInputStream()));
-			BufferedReader brerr = new BufferedReader(new InputStreamReader(
-					process.getErrorStream()));
-			String inline = null;
+  private final static Logger LOG = Logger.getLogger(ExecCommandUtils.class);
 
-			File file = new File(resultFilePath);
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-			FileWriter fw = new FileWriter(file);
-			BufferedWriter bw = new BufferedWriter(fw);
-			while (null != (inline = br.readLine())) {
-				bw.write(inline);
-				bw.newLine();
-				bw.flush(); // 将数据更新至文件
-			}
-			bw.close();
-			fw.close();
-			while (null != (inline = brerr.readLine())) {
-			}
-			br.close();
-			brerr.close();
-			exitVal = process.waitFor();
-			process.destroy();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+  /**
+   * 执行外部命令
+   * 
+   * @param command
+   */
+  public static void execCommand(final String command, String resultFilePath) {
+    try {
+      int exitVal = 53559;
 
-	/**
-	 * 监控外部命令执行情况，
-	 * 
-	 * 
-	 */
-	private static class WatchDog extends Thread {
-		private int timeout = 20000;
-		private Process target;
-		private String cmd;
+      Process process = Runtime.getRuntime().exec(command);
+      new WatchDog(process, command).start();
+      BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      BufferedReader brerr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+      String inline = null;
 
-		WatchDog(Process process, String cmd) {
-			this.target = process;
-			this.cmd = cmd;
-		}
+      File file = new File(resultFilePath);
+      if (!file.exists()) {
+        file.createNewFile();
+      }
+      FileWriter fw = new FileWriter(file);
+      BufferedWriter bw = new BufferedWriter(fw);
+      boolean isSuccess = false;
+      while (null != (inline = br.readLine())) {
+        isSuccess = true;
+        bw.write(inline);
+        bw.newLine();
+        bw.flush(); // 将数据更新至文件
+      }
+      if (!isSuccess) {
+        while (null != (inline = brerr.readLine())) {
+          bw.write(inline);
+          bw.newLine();
+          bw.flush(); // 将数据更新至文件
+        }
+      }
+      LOG.debug("isSuccess:" + isSuccess);
+      bw.close();
+      fw.close();
+      br.close();
+      brerr.close();
+      exitVal = process.waitFor();
+      process.destroy();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
-		public void run() {
-			final long start = System.currentTimeMillis();
+  /**
+   * 监控外部命令执行情况，
+   * 
+   * 
+   */
+  private static class WatchDog extends Thread {
+    private int timeout = 20000;
+    private Process target;
+    private String cmd;
 
-			while (true) {
-				if (System.currentTimeMillis() - start > timeout) {
-					target.destroy();
-					return;
-				} else {
-					try {
-						target.waitFor();
-						// target.destroy();
-						return;
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
+    WatchDog(Process process, String cmd) {
+      this.target = process;
+      this.cmd = cmd;
+    }
 
-		}
-	}
+    public void run() {
+      final long start = System.currentTimeMillis();
+
+      while (true) {
+        if (System.currentTimeMillis() - start > timeout) {
+          target.destroy();
+          return;
+        } else {
+          try {
+            target.waitFor();
+            // target.destroy();
+            return;
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }
+        try {
+          Thread.sleep(100);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+
+    }
+  }
 }
