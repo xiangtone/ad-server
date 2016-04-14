76907c150e104ad3ec238316fb0aff855a91e896
soybean217 <13565644@qq.com>
2016-04-14 09:15:45 +0800

    增加日志和FTP部署相关

--- AdService/src/main/cn/adwalker/ad/controller/PageController.java
+++ AdService/src/main/cn/adwalker/ad/controller/PageController.java
@@ -9,7 +9,6 @@
  */
 package cn.adwalker.ad.controller;
 
-
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -50,124 +49,142 @@ import cn.adwalker.ad.vo.OnlineAd;
  */
 @Controller
 public class PageController {
-	
-	@SuppressWarnings("unused")
-	private static final Logger log = LoggerFactory.getLogger(PageController.class);
-	@Resource
-	private IAdWallService adWallService;
-	//android 通知中心
-	@RequestMapping("/android/notice.do")
-	public void androidNotice(HttpServletRequest request,HttpServletResponse response){
-		ParamHandler h = ParamHandler.createParamHandler(response, request);
-		PageParam pvo = h.paramConvent(PageParam.class,"paramMap");
-		if(!h.hasError()&&VoValicate.validatePageVoForAndroid(pvo)){
-			AdverJson json = adWallService.getNoticeInfo(pvo,  AppConstant.OS_ANDROID);
-			if (json == null) {
-				PublicUtil.returnValue(response,AppConstant.STATUS_ERROR,PublicUtil.installErrorJVO(ExceptionCode.WALL_LOAD_EMPTY));
-			}else{
-				PublicUtil.returnValue(response, AppConstant.STATUS_OK, json);
-			}	
-		}else{
-			PublicUtil.returnValue(response,AppConstant.STATUS_ERROR,PublicUtil.installErrorVO(ExceptionCode.REQUEST_PARAM_INVALID));
-		}
-	}
-	
-	@RequestMapping("/android/ad_picker.do")
-	public void androidAdPicker(HttpServletRequest request,HttpServletResponse response) {
-		ParamHandler h = ParamHandler.createParamHandler(response, request);
-		WallParam pvo = h.paramConvent(WallParam.class,"paramMap");
-		if(!h.hasError()&&VoValicate.validatePageVoForAndroid(pvo)){
-			AdverJson json = adWallService.adPicker(pvo, AppConstant.OS_ANDROID);
-			if (json == null || json.getAdList() == null) {
-				PublicUtil.returnValue(response,AppConstant.STATUS_ERROR,PublicUtil.installErrorJVO(ExceptionCode.WALL_LOAD_EMPTY));
-			}else{
-				PublicUtil.returnValue(response, AppConstant.STATUS_OK, json);
-			}	
-		}else{
-			PublicUtil.returnValue(response,AppConstant.STATUS_ERROR,PublicUtil.installErrorVO(ExceptionCode.REQUEST_PARAM_INVALID));
-		}
-	}
-	
-	
-	//ios通知中心
-	@RequestMapping("/ios/notice.do")
-	public void iosNotice(HttpServletRequest request,HttpServletResponse response) {
-		ParamHandler h = ParamHandler.createParamHandler(response, request);
-		PageParam pvo = h.paramConvent(PageParam.class,AppConstant.P_PARAM_MAP_KEY);
-	    if(!h.hasError() && VoValicate.validatePageVoForIos(pvo) ){
-	    	AdverJson json = adWallService.getNoticeInfo(pvo, AppConstant.OS_IOS);
-			if (json == null) {
-				PublicUtil.returnValue(response,AppConstant.STATUS_ERROR,PublicUtil.installErrorJVO(ExceptionCode.WALL_LOAD_EMPTY));
-			}else{
-				PublicUtil.returnValue(response, AppConstant.STATUS_OK, json);
-			}	
-	    }else{
-			PublicUtil.returnValue(response,AppConstant.STATUS_ERROR,PublicUtil.installErrorVO(ExceptionCode.REQUEST_PARAM_INVALID));
-		}
-	}
-	
-	
-	//热门推荐墙
-	@RequestMapping("/ios/hot.do")
-	public void hotTueijian(HttpServletRequest request,HttpServletResponse response) {
-		ParamHandler h = ParamHandler.createParamHandler(response, request);
-		PageParam pvo = h.paramConvent(PageParam.class,AppConstant.P_PARAM_MAP_KEY);
-	    if(!h.hasError() && VoValicate.validatePageVoForIos(pvo) ){
-	    	AdverJson json = adWallService.hotTueijian(pvo, AppConstant.OS_IOS);
-			if (json == null) {
-				PublicUtil.returnValue(response,AppConstant.STATUS_ERROR,PublicUtil.installErrorJVO(ExceptionCode.WALL_LOAD_EMPTY));
-			}else{
-				PublicUtil.returnValue(response, AppConstant.STATUS_OK, json);
-			}	
-	    }else{
-			PublicUtil.returnValue(response,AppConstant.STATUS_ERROR,PublicUtil.installErrorVO(ExceptionCode.REQUEST_PARAM_INVALID));
-		}
-	}
-	
-	/**
-	 * 
-	* <p>Title: iosAdPicker</p>
-	* <p>Description:IOS广告接口</p>
-	* @param request
-	* @param response
-	* @author cuidd
-	* @date 2014年10月30日
-	* @return void
-	* @version 1.0
-	 */
-	@RequestMapping("/ios/ad_picker.do")
-	public void iosAdPicker(HttpServletRequest request,HttpServletResponse response) {
-		ParamHandler h = ParamHandler.createParamHandler(response, request);
-		WallParam pvo = h.paramConvent(WallParam.class,AppConstant.P_PARAM_MAP_KEY);
-	    if(!h.hasError() && VoValicate.validatePageVoForIos(pvo) ){
-	    	AdverJson json = adWallService.adPicker(pvo, AppConstant.OS_IOS);
-			if (json == null || json.getAdList() == null || json.getAdList().size()==0) {
-				PublicUtil.returnValue(response,AppConstant.STATUS_ERROR,PublicUtil.installErrorJVO(ExceptionCode.WALL_LOAD_EMPTY));
-			}else{
-				PublicUtil.returnValue(response, AppConstant.STATUS_OK, json);
-			}	
-	    }else{
-			PublicUtil.returnValue(response,AppConstant.STATUS_ERROR,PublicUtil.installErrorVO(ExceptionCode.REQUEST_PARAM_INVALID));
-		}
-	}
-	
-	
-	
-	@RequestMapping("/ios/ad_list.do")
-	public void iosAdList(HttpServletRequest request,HttpServletResponse response) {
-		ParamHandler h = ParamHandler.createParamHandler(response, request);
-		WallParam2 pvo = h.paramConvent(WallParam2.class,AppConstant.P_PARAM_MAP_KEY);
-	    if(!h.hasError() && VoValicate.validatePageVoForIos2(pvo) ){
-	    	List<OnlineAd>  list = adWallService.adList(pvo, AppConstant.OS_IOS);
-	    	OlineAdJson json=new OlineAdJson();
-	    	json.setRecodeList(list);
-			if (list == null ||list.size()==0) {
-				PublicUtil.returnValue(response,AppConstant.STATUS_ERROR,PublicUtil.installErrorJVO(ExceptionCode.WALL_LOAD_EMPTY));
-			}else{
-				PublicUtil.returnValue(response, AppConstant.STATUS_OK, json);
-			}	
-	    }else{
-			PublicUtil.returnValue(response,AppConstant.STATUS_ERROR,PublicUtil.installErrorVO(ExceptionCode.REQUEST_PARAM_INVALID));
-		}
-	}
+
+  @SuppressWarnings("unused")
+  private static final Logger log = LoggerFactory.getLogger(PageController.class);
+  @Resource
+  private IAdWallService adWallService;
+
+  // android 通知中心
+  @RequestMapping("/android/notice.do")
+  public void androidNotice(HttpServletRequest request, HttpServletResponse response) {
+    ParamHandler h = ParamHandler.createParamHandler(response, request);
+    PageParam pvo = h.paramConvent(PageParam.class, "paramMap");
+    if (!h.hasError() && VoValicate.validatePageVoForAndroid(pvo)) {
+      AdverJson json = adWallService.getNoticeInfo(pvo, AppConstant.OS_ANDROID);
+      if (json == null) {
+        PublicUtil.returnValue(response, AppConstant.STATUS_ERROR,
+            PublicUtil.installErrorJVO(ExceptionCode.WALL_LOAD_EMPTY));
+      } else {
+        PublicUtil.returnValue(response, AppConstant.STATUS_OK, json);
+      }
+    } else {
+      PublicUtil.returnValue(response, AppConstant.STATUS_ERROR,
+          PublicUtil.installErrorVO(ExceptionCode.REQUEST_PARAM_INVALID));
+    }
+  }
+
+  @RequestMapping("/android/ad_picker.do")
+  public void androidAdPicker(HttpServletRequest request, HttpServletResponse response) {
+    ParamHandler h = ParamHandler.createParamHandler(response, request);
+    log.info("h:" + h);
+    WallParam pvo = h.paramConvent(WallParam.class, "paramMap");
+    log.info("pvo:" + pvo);
+    if (!h.hasError() && VoValicate.validatePageVoForAndroid(pvo)) {
+      log.info("check in:");
+      AdverJson json = adWallService.adPicker(pvo, AppConstant.OS_ANDROID);
+      log.info("json:" + json);
+      if (json == null || json.getAdList() == null) {
+        PublicUtil.returnValue(response, AppConstant.STATUS_ERROR,
+            PublicUtil.installErrorJVO(ExceptionCode.WALL_LOAD_EMPTY));
+      } else {
+        PublicUtil.returnValue(response, AppConstant.STATUS_OK, json);
+      }
+    } else {
+      PublicUtil.returnValue(response, AppConstant.STATUS_ERROR,
+          PublicUtil.installErrorVO(ExceptionCode.REQUEST_PARAM_INVALID));
+    }
+  }
+
+  // ios通知中心
+  @RequestMapping("/ios/notice.do")
+  public void iosNotice(HttpServletRequest request, HttpServletResponse response) {
+    ParamHandler h = ParamHandler.createParamHandler(response, request);
+    PageParam pvo = h.paramConvent(PageParam.class, AppConstant.P_PARAM_MAP_KEY);
+    if (!h.hasError() && VoValicate.validatePageVoForIos(pvo)) {
+      AdverJson json = adWallService.getNoticeInfo(pvo, AppConstant.OS_IOS);
+      if (json == null) {
+        PublicUtil.returnValue(response, AppConstant.STATUS_ERROR,
+            PublicUtil.installErrorJVO(ExceptionCode.WALL_LOAD_EMPTY));
+      } else {
+        PublicUtil.returnValue(response, AppConstant.STATUS_OK, json);
+      }
+    } else {
+      PublicUtil.returnValue(response, AppConstant.STATUS_ERROR,
+          PublicUtil.installErrorVO(ExceptionCode.REQUEST_PARAM_INVALID));
+    }
+  }
+
+  // 热门推荐墙
+  @RequestMapping("/ios/hot.do")
+  public void hotTueijian(HttpServletRequest request, HttpServletResponse response) {
+    ParamHandler h = ParamHandler.createParamHandler(response, request);
+    PageParam pvo = h.paramConvent(PageParam.class, AppConstant.P_PARAM_MAP_KEY);
+    if (!h.hasError() && VoValicate.validatePageVoForIos(pvo)) {
+      AdverJson json = adWallService.hotTueijian(pvo, AppConstant.OS_IOS);
+      if (json == null) {
+        PublicUtil.returnValue(response, AppConstant.STATUS_ERROR,
+            PublicUtil.installErrorJVO(ExceptionCode.WALL_LOAD_EMPTY));
+      } else {
+        PublicUtil.returnValue(response, AppConstant.STATUS_OK, json);
+      }
+    } else {
+      PublicUtil.returnValue(response, AppConstant.STATUS_ERROR,
+          PublicUtil.installErrorVO(ExceptionCode.REQUEST_PARAM_INVALID));
+    }
+  }
+
+  /**
+   * 
+   * <p>
+   * Title: iosAdPicker
+   * </p>
+   * <p>
+   * Description:IOS广告接口
+   * </p>
+   * 
+   * @param request
+   * @param response
+   * @author cuidd
+   * @date 2014年10月30日
+   * @return void
+   * @version 1.0
+   */
+  @RequestMapping("/ios/ad_picker.do")
+  public void iosAdPicker(HttpServletRequest request, HttpServletResponse response) {
+    ParamHandler h = ParamHandler.createParamHandler(response, request);
+    WallParam pvo = h.paramConvent(WallParam.class, AppConstant.P_PARAM_MAP_KEY);
+    if (!h.hasError() && VoValicate.validatePageVoForIos(pvo)) {
+      AdverJson json = adWallService.adPicker(pvo, AppConstant.OS_IOS);
+      if (json == null || json.getAdList() == null || json.getAdList().size() == 0) {
+        PublicUtil.returnValue(response, AppConstant.STATUS_ERROR,
+            PublicUtil.installErrorJVO(ExceptionCode.WALL_LOAD_EMPTY));
+      } else {
+        PublicUtil.returnValue(response, AppConstant.STATUS_OK, json);
+      }
+    } else {
+      PublicUtil.returnValue(response, AppConstant.STATUS_ERROR,
+          PublicUtil.installErrorVO(ExceptionCode.REQUEST_PARAM_INVALID));
+    }
+  }
+
+  @RequestMapping("/ios/ad_list.do")
+  public void iosAdList(HttpServletRequest request, HttpServletResponse response) {
+    ParamHandler h = ParamHandler.createParamHandler(response, request);
+    WallParam2 pvo = h.paramConvent(WallParam2.class, AppConstant.P_PARAM_MAP_KEY);
+    if (!h.hasError() && VoValicate.validatePageVoForIos2(pvo)) {
+      List<OnlineAd> list = adWallService.adList(pvo, AppConstant.OS_IOS);
+      OlineAdJson json = new OlineAdJson();
+      json.setRecodeList(list);
+      if (list == null || list.size() == 0) {
+        PublicUtil.returnValue(response, AppConstant.STATUS_ERROR,
+            PublicUtil.installErrorJVO(ExceptionCode.WALL_LOAD_EMPTY));
+      } else {
+        PublicUtil.returnValue(response, AppConstant.STATUS_OK, json);
+      }
+    } else {
+      PublicUtil.returnValue(response, AppConstant.STATUS_ERROR,
+          PublicUtil.installErrorVO(ExceptionCode.REQUEST_PARAM_INVALID));
+    }
+  }
 }
--- AdService/src/main/cn/adwalker/ad/picker/util/ParamHandler.java
+++ AdService/src/main/cn/adwalker/ad/picker/util/ParamHandler.java
@@ -11,71 +11,80 @@ import cn.adwalker.ad.picker.vo.ParamVo;
 import cn.adwalker.ad.util.AppConstant;
 import cn.adwalker.ad.util.IpUtil;
 
-
-
-/**
- * @author admin
- *
- */
 public class ParamHandler {
-	@SuppressWarnings("unused")
-	private  HttpServletResponse response=null;
-	private  HttpServletRequest request=null;
-	private  boolean error=false;
-	@SuppressWarnings("unused")
-	private  String status=AppConstant.STATUS_OK;
-    
-    public ParamHandler(){}
-    public ParamHandler(HttpServletResponse response,HttpServletRequest request){
-    	this.response=response;
-    	this.request=request;
+
+  @SuppressWarnings("unused")
+  private HttpServletResponse response = null;
+  private HttpServletRequest request = null;
+  private boolean error = false;
+  @SuppressWarnings("unused")
+  private String status = AppConstant.STATUS_OK;
+
+  public ParamHandler() {
+  }
+
+  public ParamHandler(HttpServletResponse response, HttpServletRequest request) {
+    this.response = response;
+    this.request = request;
+  }
+
+  public static ParamHandler createParamHandler(HttpServletResponse response, HttpServletRequest request) {
+    return new ParamHandler(response, request);
+  }
+
+  @Override
+  public String toString() {
+    return "ParamHandler [response=" + response + ", request=" + request + ", error=" + error + ", status=" + status
+        + "]";
+  }
+
+  /**
+   * 
+   * <p>
+   * Title: paramConvent
+   * </p>
+   * <p>
+   * Description:参数转换为对象参数
+   * </p>
+   * 
+   * @param type
+   * @param paramMap
+   * @return
+   * @author cuidd
+   * @date 2014年10月31日
+   * @return T
+   * @version 1.0
+   */
+  @SuppressWarnings("unchecked")
+  public <T> T paramConvent(Class<T> type, String paramMap) {
+    Map<String, String> map = (Map<String, String>) request.getAttribute(paramMap);
+    if (map != null) {
+      BeanRequestPicker brp = new BeanRequestPicker(map);
+      ParamVo vo = (ParamVo) brp.handleMap(type);
+      vo.setIp(IpUtil.getIp(request));
+      vo.setUserAgent(request.getHeader("User-Agent"));
+      return (T) vo;
+    } else {
+      setErrorInfo(ExceptionCode.REQUEST_PARAM_INVALID);
+      return null;
     }
-    
-    public static ParamHandler createParamHandler(HttpServletResponse response,HttpServletRequest request){
-    	return new ParamHandler(response,request);
-    }
-	
-    /**
-     * 
-    * <p>Title: paramConvent</p>
-    * <p>Description:参数转换为对象参数</p>
-    * @param type
-    * @param paramMap
-    * @return
-    * @author cuidd
-    * @date 2014年10月31日
-    * @return T
-    * @version 1.0
-     */
-	@SuppressWarnings("unchecked")
-	public <T> T paramConvent(Class<T> type,String paramMap){
-		Map<String,String> map=(Map<String, String>) request.getAttribute(paramMap);
-		if(map!=null){
-			BeanRequestPicker brp = new BeanRequestPicker(map);
-			ParamVo vo = (ParamVo)brp.handleMap(type);
-			vo.setIp(IpUtil.getIp(request));
-			vo.setUserAgent(request.getHeader("User-Agent"));
-	        return (T)vo;
-		}else{
-			setErrorInfo(ExceptionCode.REQUEST_PARAM_INVALID);
-			return null;
-		}
-	}
-	@SuppressWarnings("unchecked")
-	public <T> T paramConvent(Class<T> type){
-		BeanRequestPicker brp = new BeanRequestPicker(request);
-		ParamVo vo = (ParamVo)brp.handle(type);
-		vo.setIp(IpUtil.getIp(request));
-		vo.setUserAgent(request.getHeader("User-Agent"));
-		return (T)vo;
-	}
-	
-	public boolean hasError(){
-		return error;
-	}
-	
-	private void setErrorInfo(String status){
-		this.error=true;
-		this.status=status;
-	}
+  }
+
+  @SuppressWarnings("unchecked")
+  public <T> T paramConvent(Class<T> type) {
+    BeanRequestPicker brp = new BeanRequestPicker(request);
+    ParamVo vo = (ParamVo) brp.handle(type);
+    vo.setIp(IpUtil.getIp(request));
+    vo.setUserAgent(request.getHeader("User-Agent"));
+    return (T) vo;
+  }
+
+  public boolean hasError() {
+    return error;
+  }
+
+  private void setErrorInfo(String status) {
+    this.error = true;
+    this.status = status;
+  }
 }
